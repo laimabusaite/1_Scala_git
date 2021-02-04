0567b47ce73352068232833e35ef54f4062111ae
laima <laima.busaite@gmail.com>
2021-02-04 20:15:15 +0200

    data types

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,6 +1,21 @@
 object MyApp extends App {
   println("Print line")
   println()
-  print("123")
+  println("123")
+  println()
+  val myName = readLine("What is your name?")
+  val favoriteFood = readLine(s"Hello, $myName! What is your favorite food?")
+  println(s"Cool, I like $favoriteFood as well!")
+  val currentPrice = readLine(s"How much $favoriteFood cost?").toFloat
+  val currentMoney = readLine("How much money do you have?").toFloat
+  val maxPurchaseWeight = currentMoney / currentPrice
+//  println(s"Congratulation $myName! You can buy ${(maxPurchaseWeight*100).round / 100.toDouble} kg of $favoriteFood right now.")
+  println(s"Congratulation $myName! You can buy ${(maxPurchaseWeight*100).round / 100.0} kg of $favoriteFood right now.")
+
+  val isRaining = false
+  val isSnowing = true
+  val isCold = true
+
+
 
 }
--- src/main/scala/hello.sc
+++ src/main/scala/hello.sc
@@ -8,3 +8,26 @@ print("hmmm ")
 print(math.pow(2,32))
 val b = 10
 println(a + b)
+//b = b + 4
+
+var myVar = 30
+myVar = myVar + 30
+println(myVar)
+
+val result = a + b
+val bigResult = myVar + math.pow(2,32)
+println(result.getClass, bigResult.getClass)
+
+val myName = "Laima"
+println(myName.getClass)
+
+val singleChar = 'w'
+println(singleChar.getClass)
+
+val myByte : Byte = 120
+println(myByte, myByte.getClass)
+val anotherByte : Byte = 10
+val resultByte = (myByte + anotherByte).toByte
+
+val myNumberString = "12345"
+val myNumber = myNumberString.toInt
